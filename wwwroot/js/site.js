814f8f0cd906f8d3fafcfabf958c124b12845d3a
Nick Edwards <nledwa01@gmail.com>
2022-07-23 15:09:12 -0500

    Created Landing Page. Modified Layout page. Added css and js files. Included images

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+  //Initializes the Tiny Cloud Rich Text Editor for any Text Areas with the specified class
+  tinymce.init({
+    selector: ".richtextarea",
+  });
+});
